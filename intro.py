b9741bb7dd9f17e6fd0784ae3a1a787fb4aa5720
CudraMohamed <ncudramohamed@gmail.com>
2022-04-03 13:20:23 +0300

    Intro commit

---
+++ intro.py
@@ -0,0 +1,49 @@
+#Variables
+
+msg="Hello Python World"
+print(msg)
+print("Hello Nick")
+wallet=41
+print(wallet)
+wallet=32
+#there is no output if you don"t print
+
+
+#Make a variable called day and set it equal to the date of the month
+day=3
+print(day)
+
+#Types
+
+#Int
+day=3
+temp=-15
+print(day)
+print(temp)
+
+#Float
+weight=190.4
+weight=20.4567890
+print(weight)
+#Calculation
+#addition
+print(3+6)
+
+#adding variables to Int
+print(day+3)
+
+#multiplication
+print(weight*2)
+
+#division
+print(weight/2)
+
+#subtraction
+print(9-4)
+
+#Find something around you in your life to represent
+#an Int and a Float.Put them in variables
+chairs=35
+print(chairs)
+tv=3
+print(tv)
